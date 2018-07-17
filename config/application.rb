66680fd86767b80edebdf6448d535c7aa5a9bbf0
mklarman <mklarman@hotmail.com>
2018-07-17 09:14:43 -0400

    launching fixes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MyCompuPick
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # added the below line to fix error on heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
